8c0f0484e8a7f08c9f56d493cd2bf9223cf32b30
ButrintBytyqi <butrinti022@gmail.com>
2022-07-19 20:48:26 +0200

    Rregullimi i pjeses se pare

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import "./App.css";
+import "./fonts/Orbitron-VariableFont_wght.ttf";
+import cv from "./downloads/FinalCVButrintBytyqi.pdf";
 
 function App() {
   return (
     <>
-      <div>Hello World</div>;
+      <div className="container">
+        <div className="topNav">
+          <nav>
+            <ul>
+              <li>
+                <a href="">home</a>
+              </li>
+              <li>
+                <a href="">about me</a>
+              </li>
+              <li>
+                <a href="">projects</a>
+              </li>
+              <li>
+                <a href="">contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="section-right">
+          <div className="section-home">
+            <h1>I am Butrint Bytyqi.</h1>
+            <h5>Front End & React Developer</h5>
+            <hr />
+            <p>
+              From day one, technology has fascinated me. It was something I
+              would never tire of doing. I have always loved creating websites
+              and apps!
+            </p>
+            <a href={cv} download>
+              Download CV
+            </a>
+          </div>
+
+          <div className="section-aboutMe">
+            <h1>About Me</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
