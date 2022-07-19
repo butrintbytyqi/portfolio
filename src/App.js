6c6d7852100962ddb427f3564b8cb2fa528439e4
ButrintBytyqi <butrinti022@gmail.com>
2022-07-19 23:53:33 +0200

    Perfundimi

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import "./App.css";
 import "./fonts/Orbitron-VariableFont_wght.ttf";
 import cv from "./downloads/FinalCVButrintBytyqi.pdf";
+import upz from "./img/upz.png";
+import makerspace from "./img/makerspace.png";
+import jcoders from "./img/jcoders.png";
+import reactLogo from "./img/reactLogo.png";
 
 function App() {
   return (
     <>
       <div className="container">
         <div className="topNav">
-          <nav>
-            <ul>
-              <li>
-                <a href="">home</a>
-              </li>
-              <li>
-                <a href="">about me</a>
-              </li>
-              <li>
-                <a href="">projects</a>
-              </li>
-              <li>
-                <a href="">contact</a>
-              </li>
-            </ul>
-          </nav>
+          <ul>
+            <li>
+              <a href="#home">home</a>
+            </li>
+            <li>
+              <a href="#aboutme">about me</a>
+            </li>
+            <li>
+              <a href="#projects">projects</a>
+            </li>
+            <li>
+              <a href="#contact">contact</a>
+            </li>
+          </ul>
         </div>
+
         <div className="section-right">
-          <div className="section-home">
+          <div className="section-home" id="home">
             <h1>I am Butrint Bytyqi.</h1>
             <h5>Front End & React Developer</h5>
             <hr />
@@ -39,9 +42,111 @@ function App() {
             </a>
           </div>
 
-          <div className="section-aboutMe">
+          <div className="section-aboutMe" id="aboutme">
             <h1>About Me</h1>
+            <div className="cards">
+              <div className="card1">
+                <img src={upz} alt="" />
+                <h1>University "Ukshin Hoti" Prizren </h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                  non laudantium minus. Repellendus, itaque reprehenderit! Odio
+                  enim sed ipsam eius laboriosam asperiores numquam, similique
+                  fuga, ducimus mollitia facere est. Modi!
+                </p>
+              </div>
+              <div className="card1">
+                <img src={makerspace} alt="" />
+                <h1>MakerSpace Innovation Center</h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                  non laudantium minus. Repellendus, itaque reprehenderit! Odio
+                  enim sed ipsam eius laboriosam asperiores numquam, similique
+                  fuga, ducimus mollitia facere est. Modi!
+                </p>
+              </div>
+              <div className="card1">
+                <img src={jcoders} alt="" />
+                <h1>jCoders Academy </h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                  non laudantium minus. Repellendus, itaque reprehenderit! Odio
+                  enim sed ipsam eius laboriosam asperiores numquam, similique
+                  fuga, ducimus mollitia facere est. Modi!
+                </p>
+              </div>
+            </div>
           </div>
+
+          <div className="section-projects" id="projects">
+            <h1>Projects</h1>
+            <div className="project-cards">
+              <div className="github-card">
+                <a href="https://github.com/butrintbytyqi/Feedback-App.git">
+                  Feedback-App
+                </a>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+                  veniam quae tenetur beatae minima odit veritatis, culpa eum et
+                  impedit.
+                </p>
+                <div className="react-logo">
+                  <img src={reactLogo} alt="" />
+                  <p>Made with React</p>
+                </div>
+              </div>
+              <div className="github-card">
+                <a href="https://github.com/butrintbytyqi/Feedback-App.git">
+                  Feedback-App
+                </a>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+                  veniam quae tenetur beatae minima odit veritatis, culpa eum et
+                  impedit.
+                </p>
+                <div className="react-logo">
+                  <img src={reactLogo} alt="" />
+                  <p>Made with React</p>
+                </div>
+              </div>
+              <div className="github-card">
+                <a href="https://github.com/butrintbytyqi/Feedback-App.git">
+                  Feedback-App
+                </a>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+                  veniam quae tenetur beatae minima odit veritatis, culpa eum et
+                  impedit.
+                </p>
+                <div className="react-logo">
+                  <img src={reactLogo} alt="" />
+                  <p>Made with React</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="section-contact" id="contact">
+            <h1>Contact</h1>
+            <div className="inputs">
+              <p>Let's meet! Send a message</p>
+              <input type="name" username="name" placeholder="Name"></input>
+              <input type="email" name="email" placeholder="Email" />
+              <input
+                type="subject"
+                username="subject"
+                placeholder="Subject"
+              ></input>
+              <textarea type="text" name="message" placeholder="Message" />
+              <button>Send Message</button>
+            </div>
+          </div>
+
+          <footer>
+            Copyright Protected © <br />
+            <br />
+            Powered by Butrint Bytyqi
+          </footer>
         </div>
       </div>
     </>
